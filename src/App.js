7cdc586068ee9c8295709a00eff87acdb88cf276
vntero <shin.antero@gmail.com>
2022-02-01 15:17:03 +0100

    map function working as expected

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import Header from './components/Header';
 import Note from './components/Note';
 import Footer from './components/Footer';
+import notes from './notes';
+
+function createNotes(eachNote) {
+  return <Note 
+    key={eachNote.key}
+    title={eachNote.title}
+    content={eachNote.content}
+  />
+}
 
 function App() {
   return (
     <div>
       <Header/>
-      <Note/>
+      {notes.map(createNotes)}
       <Footer/>
     </div>
   );
